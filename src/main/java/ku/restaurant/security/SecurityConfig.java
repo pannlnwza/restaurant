d67d79104c2dfb4be1067a07dfe666d01978e9d5
Pattapon Gowanit <iampattapon@gmail.com>
2025-11-06 14:24:48 +0700

    Finished the Authorization lab

--- src/main/java/ku/restaurant/security/SecurityConfig.java
+++ src/main/java/ku/restaurant/security/SecurityConfig.java
@@ -4,6 +4,7 @@ package ku.restaurant.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -68,6 +69,14 @@ public class SecurityConfig {
                         authorizeRequests
                                 // Our public endpoints
                                 .requestMatchers("/api/auth/**").permitAll()
+
+                                // Role-based endpoints
+                                .requestMatchers(HttpMethod.GET, "/api/restaurants/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+                                .requestMatchers(HttpMethod.POST, "/api/restaurants").hasAnyAuthority("ROLE_ADMIN")
+                                .requestMatchers(HttpMethod.PUT, "/api/restaurants").hasAnyAuthority("ROLE_ADMIN")
+                                .requestMatchers(HttpMethod.DELETE, "/api/restaurants").hasAnyAuthority("ROLE_ADMIN")
+                                .requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+
 
 
                                 // All other endpoints require authentication
